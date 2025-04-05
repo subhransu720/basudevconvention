2a266541400f5da5c385d875db4d5908195e9c67
Subhransu Swain <your-email@example.com>
2025-04-05 12:26:07 +0530

    Fix image loading and resource handling for production

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     domains: ['localhost', 'basudevconvention.vercel.app'],
+    unoptimized: true,
   },
   poweredByHeader: false,
   compress: true,
